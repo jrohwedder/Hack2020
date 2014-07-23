b87bdf628e796ecc199c2685591fba5d86965630
Thomas Barker <barker.tommy@gmail.com>
2014-07-23 16:33:20 -0600

    updated Ross's JS file

--- Ross/Hack2020/sticky.js
+++ Ross/Hack2020/sticky.js
@@ -1,6 +1,7 @@
 /*Creates new sticky on screen*/
 function createSticky(){
-    var sticky1 = document.createElement('p');
+    var sticky1 = document.createElement("p");
+    sticky1.setAttribute("class","sticky_note");
     sticky1.innerHTML="I'm a sticky.<br>Look at me.";
-    document.getElement("body").appendChild(sticky1);
+    document.body.appendChild(sticky1);
 }
